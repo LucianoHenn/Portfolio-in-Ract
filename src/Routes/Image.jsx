884ca90c97196711ace61fbff9a2259194f1a37a
LucianoHenn <79486565+LucianoHenn@users.noreply.github.com>
2021-05-17 22:09:18 -0300

    Update Image.jsx

--- src/Routes/Image.jsx
+++ src/Routes/Image.jsx
@@ -37,7 +37,9 @@ function Image(){
    
 
     return(<>
-  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+ <meta name="viewport" content="width=device-width" />
+ <meta name="viewport" content="initial-scale=1.0" />
+
     { 
             band ? 
     <>
